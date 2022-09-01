ac15341cce28ecbe23edcc48ad8a8d518ab3f03f
Joao Ricardo Soares da Silva <98894124+Rico1882@users.noreply.github.com>
2022-09-01 19:52:37 -0300

    Estruturas e comandos personalizados

--- cypress/integration/usuarios/usuarios.spec.js
+++ cypress/integration/usuarios/usuarios.spec.js
@@ -11,20 +11,29 @@ describe('Casos de teste sobre a rota /usurios da API Serverest', () => {
     })
 
     it('Não deve postar um novo usuário administrador existente', () => {
-        cy.request({
-            method: 'POST',
-            url: '/usuarios',
-            failOnStatusCode: false,
-            body: {
-                "nome": "Fulano da Silva",
-                "email": "beltrano@qa.com.br",
-                "password": "teste",
-                "administrador": "true"
-            }
-        }).then( res => {
+        cy.postarUsuarioSemSucesso().then( res => {
             expect(res).to.be.a('object')
             expect(res.body.message).to.be.a('string')  
             expect(res.body.message).to.be.eq('Este email já está sendo usado')
         })      
     })
-  })
+
+    it('Deve validar o comando personalisado', () => {
+        cy.rest('GET', '/usuarios').then( res => {
+            expect(res).to.be.a('object')
+            cy.log(JSON.stringify(res))
+        })
+    })
+    it.only('Deve realizar login com suscesso', () => {
+        cy.buscarUsuarioParaLogin().then(usuario => {
+           cy.logar(usuario.email, usuario.senha).then( res=> {
+                expect(res).to.be.a('object')
+                expect(res.body.message).to.be.a('string')
+                expect(res.body).to.haveOwnProperty('authorization')
+                var bearer = res.body.authorization.slice(7)
+                cy.log(bearer)
+           })
+        }) 
+       
+})
+})
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -7,12 +7,6 @@
 // commands please read more here:
 // https://on.cypress.io/custom-commands
 // ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
 // -- This is a child command --
 // Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
 //
@@ -23,3 +17,52 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+
+
+
+
+Cypress.Commands.add('postarUsuarioSemSucesso', () => { 
+    return cy.request({
+        method: 'POST',
+        url: '/usuarios',
+        failOnStatusCode: false,
+        body: {
+            "nome": "Fulano da Silva",
+            "email": "beltrano@qa.com.br",
+            "password": "teste",
+            "administrador": "true"
+        }
+})
+})
+
+Cypress.Commands.add('rest', (method = 'GET', url = '/', body = null, failOnStatusCode = false) => {
+    return cy.request({
+        method: method,
+        url: url,
+        failOnStatusCode: failOnStatusCode,
+        body: body
+})
+})
+Cypress.Commands.add('logar', (email, senha) =>{
+    return cy.request({
+        method: 'POST',
+        url: '/login',
+        failOnStatusCode: false,
+        body: {
+            "email": email,
+            "password": senha 
+        }
+
+    })
+})
+
+Cypress.Commands.add('buscarUsuarioParaLogin', () =>{
+    cy.rest('GET', '/usuarios').then( res => {
+        expect(res.body).to.haveOwnProperty('usuarios')
+        return {
+            email: res.body.usuarios[0].email,
+            senha: res.body.usuarios[0].password
+            }
+        })
+})
